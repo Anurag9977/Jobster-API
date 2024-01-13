242e5ab375249e90d281fa421d49f5a70be245fb
ANURAG CHOUDHURY <anuragchoudhury852@gmail.com>
2024-01-13 12:42:33 +0530

    Updated

--- mock-data/populate.js
+++ mock-data/populate.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
 const Job = require("../model/jobs");
 const mockData = require("./mock_jobs.json");
-const url =
-  "mongodb+srv://Anurag:Anuragmongodb%402023@task-manager.exkdxz8.mongodb.net/jobster-api?retryWrites=true&w=majority";
+const url = "###";
 const connectToDB = async (mongo_url) => {
   await mongoose.connect(mongo_url);
   console.log("DB Connected for Population");
